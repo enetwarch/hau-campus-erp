d43be1b878deadf3595acc0f8dc95a9047c19f12
Enetwarch <enetwarch@protonmail.com>
2025-03-20 14:40:58 +0800

    minor changes to index routing

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ window.addEventListener("load", () => {
     const loginForm = document.getElementById("loginForm");
     loginForm.addEventListener("submit", event => {
         event.preventDefault();
-        window.location.href = "dashboard.html";
+        window.location.href = "dashboard";
     });
 });

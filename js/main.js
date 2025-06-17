2b65aefbaaa09769b331579660ecb574ce6fefc6
Enetwarch <enetwarch@protonmail.com>
2025-06-17 21:34:28 +0800

    refactor: replaced dynamic warning from js to html

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-import Warning from "./modules/warning.js";
 import UI from "./controllers/ui.js";
 
 window.addEventListener("load", () => {
-    const media = window.matchMedia("(min-width: 1537px)");
-    const iconClass = "fa-desktop";
-    const message = "Please view on desktop";
-
-    const warning = new Warning(media, iconClass, message);
-
     const page = document.body.dataset.page;
     const ui = new UI(page);
 });

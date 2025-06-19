4742f58eec65e3c831faab7a339ddc7741930313
Enetwarch <enetwarch@protonmail.com>
2025-06-19 09:56:13 +0800

    feat: added 404 not found page

---
+++ js/404.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const timeoutId = setTimeout(() => {
+  const redirectionLink = document.createElement("a");
+    redirectionLink.href = "./";
+
+    redirectionLink.click();    
+  }, 3000);
+
+  window.addEventListener("beforeunload", () => clearTimeout(timeoutId));
+});

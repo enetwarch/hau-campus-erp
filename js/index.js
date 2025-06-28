c548280bd20e562790a4377a91eb0446abf3dcf8
Enetwarch <enetwarch@protonmail.com>
2025-06-28 19:58:04 +0800

    style: changed tabwidth indent from 2 to 4

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const controller = new AbortController();
-  const signal = controller.signal;
+    const controller = new AbortController();
+    const signal = controller.signal;
 
-  const loginFormElement = document.getElementById("login-form");
-  loginFormElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const loginFormElement = document.getElementById("login-form");
+    loginFormElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    const redirectionLink = document.createElement("a");
-    redirectionLink.href = "./dashboard";
+        const redirectionLink = document.createElement("a");
+        redirectionLink.href = "./dashboard";
 
-    redirectionLink.click();
-  }, { signal });
+        redirectionLink.click();
+    }, { signal });
 
-  window.addEventListener("beforeunload", () => controller.abort());
+    window.addEventListener("beforeunload", () => controller.abort());
 });
